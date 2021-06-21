31a9fa22fca4c962e7a169e99c1c33cceeb07fc7
Nicolas Cabrera Lettiere <nicolas.cabrera.lettiere@gmail.com>
2021-06-20 23:35:52 -0300

    example pictures for README

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = require('./build/Release/filemanager.node');
+var filemanager = undefined;
+
+if(process.platform == "win32" && process.arch == "x64")
+	filemanager = require('./bin/win/x64/Release/filemanager.node'); 
+else if(process.platform == "win32" && process.arch == "ia32")
+	filemanager = require('./bin/win/x86/Release/filemanager.node');
+else
+	filemanager = require('./build/Release/filemanager.node');
+
+module.exports = filemanager;
--- test.js
+++
@@ -1,2 +0,0 @@
-const filemanager = require('./build/Release/filemanager.node');
-console.log(filemanager.select_folder_dialogue());
