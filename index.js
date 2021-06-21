bf3d629045703930b01166a2deef2f53570cd03e
Nicolas Cabrera Lettiere <nicolas.cabrera.lettiere@gmail.com>
2021-06-21 03:06:46 -0300

    Removed unused files.

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-var filemanager = undefined;
-
-if(process.platform == "win32" && process.arch == "x64")
-	filemanager = require('./bin/win/x64/Release/filemanager.node'); 
-else if(process.platform == "win32" && process.arch == "ia32")
-	filemanager = require('./bin/win/x86/Release/filemanager.node');
-else
-	filemanager = require('./build/Release/filemanager.node');
-
-module.exports = filemanager;
+const filemanager = require('./build/Release/filemanager.node');
+module.exports.filemanager = filemanager;
--- test/test.js
+++ test/test.js
@@ -1,15 +1,2 @@
-var filemanager = undefined;
-// Load the precompiled binary for windows.
-if(process.platform == "win32" && process.arch == "x64") {
-	filemanager = require('../bin/win/x64/Release/filemanager.node');  
-    console.log("Using x64 pre-built binary");
-} else if(process.platform == "win32" && process.arch == "ia32") {
-	filemanager = require('../bin/win/x86/Release/filemanager.node'); 
-    console.log("Using x86 pre-built binary");
-} else {
-	// Load the new built binary for other platforms.
-	filemanager = require('../build/Release/filemanager.node');  
-    console.log("Using local build binary.");
-}
-
+const filemanager = require('./build/Release/filemanager.node');
 console.log(filemanager?.select_folder_dialogue());
