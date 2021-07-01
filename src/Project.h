f67f5873669ede4e8471cf431970b88555349f64
Nicolas Cabrera Lettiere <nicolas.cabrera.lettiere@gmail.com>
2021-07-01 04:29:15 -0300

    Project header

---
+++ src/Project.h
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace rus {
+    class Project
+    {
+    private:
+        /* data */
+    public:
+        Project(/* args */);
+        ~Project();
+    };
+    
+    Project::Project(/* args */)
+    {
+    }
+    
+    Project::~Project()
+    {
+    }
+}
